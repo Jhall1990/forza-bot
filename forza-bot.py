2fdcac8e6e7d7e687fff5da74a40dabefa28abd4
Jacob Hall <jacobhall90@gmail.com>
2022-04-20 20:24:52 +0000

    Added track/championship support, I think

--- forza-bot.py
+++ forza-bot.py
@@ -15,8 +15,10 @@ random-car <type> - pick a random car from the type
 """
 
 CAR_CSV = "cars.csv"
+TRACK_CSV = "tracks.csv"
 PI_CLASSES = ["D", "C", "B", "A", "S1", "S2", "X"]
 RACE_TYPES = ["RALLY", "STREET", "OFFROAD"]
+TRACK_IMAGE = "https://cdn.guides4gamers.com/sites/28/screenshots/2021/12/1920/{}.jpg"
 
 with open("token.txt", "r") as token_file:
     TOKEN = token_file.read().strip()
@@ -55,12 +57,34 @@ class Car(object):
     def from_csv_row(row):
         return Car(*row)
 
+class Track(object):
+    def __init__(self, name, surface, type_):
+        self.name = name
+        self.surface = surface
+        self.type = type_
+        self.image_url = TRACK_IMAGE.format("-".join(self.name.split()))
+
+    def __str__(self):
+        return ("```Name:    {}\n"
+                "Surface: {}\n"
+                "Type:    {}\n```"
+                "{}".format(self.name.title(),
+                                     self.surface.title(),
+                                     self.type.title(),
+                                     self.image_url))
+
+    @staticmethod
+    def from_csv_row(row):
+        return Track(*row)
+
 class ForzaBotClient(discord.Client):
-    def __init__(self, cars):
+    def __init__(self, cars, tracks):
         super(ForzaBotClient, self).__init__()
         self.cars = cars
         self.car_types = [i for i in self.cars.keys()]
         self.all_cars = self.get_all_cars()
+        self.tracks = tracks
+        self.all_tracks = self.get_all_tracks()
         self.cmd_str = "/forza"
 
     async def on_ready(self):
@@ -86,12 +110,23 @@ class ForzaBotClient(discord.Client):
                 all_cars.append(car)
         return all_cars
 
+    def get_all_tracks(self):
+        all_tracks = []
+        for track_list in self.tracks.values():
+            for track in track_list:
+                all_tracks.append(track)
+        return all_tracks
+
     def message_handler(self, message):
         cmds = {
                     "test": self.handle_test,
                     "new-car": self.handle_new_car,
                     "list": self.handle_list,
                     "random": self.handle_random_car,
+                    "track": self.handle_track,
+                    "championship": self.handle_championship,
+                    "champ": self.handle_championship,
+                    "ship": self.handle_championship,
                     "help": self.handle_help,
                     "halp": self.handle_help,
                }
@@ -101,7 +136,7 @@ class ForzaBotClient(discord.Client):
         handle = cmds.get(cmd[0])
 
         if handle:
-            return "```{}```".format(handle(cmd))
+            return "{}".format(handle(cmd))
         return "Command not supported"
 
     def handle_test(self, cmd):
@@ -119,7 +154,7 @@ class ForzaBotClient(discord.Client):
         random.shuffle(filtered_types)
         perf_class = filtered_types[0]
 
-        return "Type: {}\nClass: {}\nRace: {}".format(car_type, perf_class, race_type)
+        return "```Type: {}\nClass: {}\nRace: {}```".format(car_type, perf_class, race_type)
 
     def handle_list(self, cmd):
         if len(cmd) == 1:
@@ -128,9 +163,9 @@ class ForzaBotClient(discord.Client):
         list_cmd = " ".join(cmd[1::]).lower()
 
         if cmd[1] == "types":
-            return "\n".join(sorted(self.car_types))
+            return "```" + "\n".join(sorted(self.car_types)) + "```"
         if list_cmd in self.car_types:
-            return "\n".join([str(i) for i in self.cars[list_cmd]])
+            return "```" + "\n".join([str(i) for i in self.cars[list_cmd]]) + "```"
         return "I don't recognize that car type sorry"
 
     def handle_random_car(self, cmd):
@@ -141,11 +176,54 @@ class ForzaBotClient(discord.Client):
         random_cmd = " ".join(cmd[1::]).lower()
         if random_cmd in self.car_types:
             rand_idx = random.randint(0, len(self.cars[random_cmd]))
-            return str(self.cars[random_cmd][rand_idx])
+            return "```" + str(self.cars[random_cmd][rand_idx]) + "````"
         return "I don't recognize that car type sorry"
 
+    def handle_track(self, cmd):
+        if len(cmd) == 1:
+            random.shuffle(self.all_tracks)
+            return(str(self.all_tracks[0]))
+
+        surface = cmd[1].lower()
+        if surface in self.tracks:
+            rand_idx = random.randint(0, len(self.tracks[surface]))
+            return str(self.tracks[surface][rand_idx])
+        return "I don't recognize that surface sorry.\n\nValid surfaces: road, rally, offroad"
+
+    def handle_championship(self, cmd):
+        if len(cmd) == 1:
+            num_races = 3
+            surface = None
+        elif len(cmd) == 2:
+            if cmd[1].isdigit():
+                num_races = int(cmd[1])
+                surface = None
+            else:
+                num_races = 3
+                surface = cmd[1].lower()
+        elif len(cmd) == 3:
+            if cmd[1].isdigit():
+                num_races = int(cmd[1])
+                surface = cmd[2].lower()
+            elif cmd[2].isdigit():
+                num_races = int(cmd[2])
+                surface = cmd[1].lower()
+
+        if num_races == 69:
+            return "NICE"
+
+        if surface and surface not in self.tracks:
+            return "I don't recognize that surface sorry.\n\nValid surfaces: road, rally, offroad"
+
+        if surface:
+            random.shuffle(self.tracks[surface])
+            return "\n\n".join(str(i) for i in self.tracks[surface][0:num_races])
+
+        random.shuffle(self.all_tracks)
+        return "\n\n".join(str(i) for i in self.all_tracks[0:num_races])
+
     def handle_help(self, cmd):
-        return """Here's all the commands I support
+        return """```Here's all the commands I support
 
 * new-car - Prints a random car type, class, and PI. 
 
@@ -153,11 +231,16 @@ class ForzaBotClient(discord.Client):
 
 * random [car-type] - Print a random car. If [car-type] is provided instead print a random car of that type.
 
+* track [surface] - Print a random track. If [surface] is provided only tracks with that surface will be included .
+
+* championship [num_races] [surface] - Create a championship with [num_races] with type [surface]. Default 3 races any type.
+
 * help - Print this help text, again.
+```
         """
 
 
-def parse_csv(csv_path):
+def parse_cars(csv_path):
     cars = {}
 
     with open(csv_path, "r") as csv_file:
@@ -174,6 +257,23 @@ def parse_csv(csv_path):
     return cars
 
 
+def parse_tracks(csv_path):
+    tracks = {}
+
+    with open(csv_path, "r") as csv_file:
+        track_reader = csv.reader(csv_file, delimiter=",")
+
+        for index, row in enumerate(track_reader):
+            if index == 0:
+                continue
+
+            track = Track.from_csv_row(row)
+            if track.surface not in tracks:
+                tracks[track.surface] = []
+            tracks[track.surface].append(track)
+    return tracks
+
+
 def lowest_class(cars):
     lowest = len(PI_CLASSES) - 1
 
@@ -186,12 +286,13 @@ def lowest_class(cars):
 
 
 if __name__ == "__main__":
-    cars = parse_csv(CAR_CSV)
+    cars = parse_cars(CAR_CSV)
+    tracks = parse_tracks(TRACK_CSV)
 
     for car_type, car_list in cars.items():
         print("Type: {}".format(car_type))
         for car in car_list:
             print("   {}".format(str(car)))
 
-    client = ForzaBotClient(cars)
+    client = ForzaBotClient(cars, tracks)
     client.run(TOKEN)
